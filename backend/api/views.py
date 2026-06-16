b4c89cb1559547801a9fd4892a192811e33d6661
rk <rk@163.com>
2026-06-16 11:07:43 +0800

    111111

--- backend/api/views.py
+++ backend/api/views.py
@@ -29,8 +29,8 @@ class LoginView(APIView):
         refresh = RefreshToken.for_user(user)
         user_data = UserSerializer(user).data
         role_data = {
-            'code': user.role,
-            'name': user.get_role_display()
+            'code': 'admin',
+            'name': '管理员'
         }
 
         return Response({
